7ea5e3e0c7a6f5dec30e59beb3f84cf168a9208e
Yun Sung Choi <cyss0317@gmail.com>
2021-11-18 10:03:06 -0600

    Adding try and catch for the requests

--- frontend/src/util/todo_util.js
+++ frontend/src/util/todo_util.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 export const createTodo = async data => {
-    const res = await axios.post('/api/todos/create', data)
-    return res
+    try{
+        const res = await axios.post('/api/todos/create', data)
+        return res
+    } catch(err){
+        alert("Create request wasn't succesfully done, please try again")
+        alert(err)
+    }
 }
 
 export const updateTodo = async todo => {
@@ -10,22 +15,37 @@ export const updateTodo = async todo => {
         const res = await axios.put(`/api/todos/${todo.id}`, todo)
         return res
     } catch(err){
-        alert("Something went wrong, please try again")
+        alert("Update request wasn't successfully done, please try again")
         alert(err)
     }
 }
 
 export const deleteTodo = async todoId => {
-    const res = await axios.delete(`/api/todos/${todoId}`)
-    return res
+    try{
+        const res = await axios.delete(`/api/todos/${todoId}`)
+        return res
+    } catch(err){
+        alert("Delete request wasn't successfully done, please try again")
+        alert(err)
+    }
 }
 
 export const getTodo = async todoId => {
-    const res = await axios.get(`/api/todos/${todoId}`)
-    return res
+    try{
+        const res = await axios.get(`/api/todos/${todoId}`)
+        return res
+    } catch(err){
+        alert("Couldn't find the todo, please try again")
+        alert(err)
+    }
 }
 
 export const getTodos = async () => {
-    const res = await axios.get(`/api/todos/`)
-    return res
+    try{
+        const res = await axios.get(`/api/todos/`)
+        return res
+    } catch(err){
+        alert("Couldn't fetch todos, please try again")
+        alert(err)
+    }
 }
