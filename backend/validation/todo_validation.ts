09e37f584e151730d9bc0e3b49ea87ef12a477eb
Yun Choi <cyss0317@gmail.com>
2022-08-20 21:15:40 -0500

    Rename the backend codes

---
+++ backend/validation/todo_validation.ts
@@ -0,0 +1,21 @@
+import { Todo } from "../todos/types";
+
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+interface Error {
+  description?: string;
+}
+
+module.exports = function validateRegisterTodo(data: Todo) {
+  let errors: Error = {};
+  data.description = validText(data.description) ? data.description : "";
+  if (Validator.isEmpty(data.description)) {
+    errors.description = "Description field is required";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0,
+  };
+};
